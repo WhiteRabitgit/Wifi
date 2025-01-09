9bc2c707e03fc0ce993f9454e9bf4d3dc344e438
WhiteRabitgit <119157251+WhiteRabitgit@users.noreply.github.com>
2025-01-10 00:01:25 +0530

    Add files via upload

---
+++ wifilooker.ps1
@@ -0,0 +1,68 @@
+﻿$header = @"
+
+
+
+░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
+░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░     
+                                        
+                                        
+                                                                                         
+
+
+"@
+
+Write-Host $header -ForegroundColor DarkGreen
+
+Write-Host "> Finding saved password 1"
+[int]$read = Read-Host "`n> Enter the number "
+
+try{
+
+$read -eq 1
+$names = netsh wlan show profile | Select-string "All User Profile" | ForEach-Object {($_ -split ":")[1].trim()}
+
+$count = 1
+
+
+foreach($ssid in $names){
+
+Write-Host "$count.$ssid"
+$count++
+}
+
+[int]$read = Read-Host "`nEnter the number for the profile "
+if($read -gt 0 -and $read -le $names.Count)
+{
+
+
+$checkingOffset = $names[$read - 1]
+
+
+$Keyforpwd = netsh wlan show profile $checkingOffset key=clear | select-string "Key Content"
+
+
+Write-Host "`n$checkingOffset : $(($Keyforpwd -replace "Key Content *:",'' ).trim())" -ForegroundColor Green
+
+}
+else
+{
+Write-Host "`nInvalid selection $_" -ForegroundColor DarkRed
+}
+
+
+
+}
+
+catch{
+
+Write-Host "Error $_"
+
+}
+
+
+
